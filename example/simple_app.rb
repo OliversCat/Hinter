c8a64c364f8e481c47f27a3d0475d8874574f2ab
Oliver Yu <oliver.yu@rsa.com>
2016-11-15 17:27:08 +0800

    update file structure

--- example/simple_app.rb
+++ example/simple_app.rb
@@ -1,3 +1,56 @@
-require 'hints'
+require 'hintrb'
 
+class User
+  include Hint
 
+  #[:filter]
+  def __rootscope
+    unless request.path == '/user/auth' then
+        unless token = request.env["HTTP_AUTH_TOKEN"] then
+          halt 401
+        else
+          unless Service::System.validate_fingerprint(token, request.user_agent << request.ip)
+            halt 401
+          end
+        end
+    end
+  end
+
+  #[:rpost]
+  def auth(uid, upass)
+      if user = Service::User.get(uid) then
+          if Service::User.auth(uid, upass) then
+              token = Service::System.newtoken(request.user_agent << request.ip)
+              return Result.Success do
+                {token: token}
+              end
+          else
+              return Result.Fail
+          end
+      else
+          return Result.Fail
+      end
+  end
+
+  #[:rget]
+  def info(uid)
+      if user = Service::User.get(uid) then
+        Result.Success do
+          {uid: uid, roles: user.roles, last_update_date: user.last_update_date}
+        end
+      else
+        return Result.Fail
+      end
+  end
+
+  #[:rput]
+  def create(uid, upass, roles)
+      return Result.Fail("User already existed") if Service::User.get(uid)
+      if Service::User.create({uid: uid, upass: upass}) then
+          return Result.Success
+      else
+          return Result.Fail
+      end
+  end
+
+end
--- lib/hints.rb
+++
@@ -1,2 +0,0 @@
-#$:.unshift File.dirname(__FILE__)
-require 'hints/sinatra'
